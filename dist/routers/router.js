ccf997d835def8855d0ac421f9146b96daa35471
aceon2ndserve <stelios.zoit@gmail.com>
2023-02-08 21:35:00 +0200

    first commit

---
+++ dist/routers/router.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import fetch from "node-fetch";
+export const router = Router();
+router.post("/", async (req, res) => {
+    const address = req.body.address;
+    const city = req.body.city;
+    const postalCode = req.body.postalCode;
+    const data = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}+${city}+${postalCode}&key=${process.env.GOOGLE_API}`);
+    if (!data.ok) {
+        /* Handle */
+    }
+    // If you care about a response:
+    if (data.body !== null) {
+        // body is ReadableStream<Uint8Array>
+        // parse as needed, e.g. reading directly, or
+        // and further:
+        // const finalData = await data.json().then((data) => console.log(data));
+        const finalData = await data.json();
+        if (finalData.status === "ZERO_RESULTS" ||
+            finalData.results[0].geometry.location_type === "APPROXIMATE" ||
+            finalData.results[0].address_components[0].types[0] !== "street_number") {
+            res.send("please enter a valid address");
+            return;
+        }
+        // console.log(finalData);
+        res.send(`Your address is ${finalData.results[0].formatted_address}`);
+    }
+});
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { router } from "../routers/router.js";
+dotenv.config();
+const port = 8080;
+const app = express();
+
+app.use(express.json());
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
