7b71aea7312ab26bb52c8c2e8b77f85a4d005b0f
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2023-02-11 14:50:15 +0200

    Project is now running as from source code

--- src/routers/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import fetch from "node-fetch";
-import { addressCallback } from "../services/address-validation/index.js";
-export const router = Router();
-
-
-router.post("/", addressCallback);
