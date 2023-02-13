a627d1a42da758179cf7c72c484986a1415ffa77
aceon2ndserve <stelios.zoit@gmail.com>
2023-02-13 17:53:56 +0200

    Adds sqlite3

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,37 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { addressController } from './controllers/address';
-
-dotenv.config();
+import { Sequelize, DataTypes } from 'sequelize';
 const port = 8080;
+dotenv.config();
+
+const sequelize = new Sequelize('database', process.env.USER!, process.env.PASSWORD, {
+  host: '0.0.0.0',
+  dialect: 'sqlite',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  storage: './src/db/database.sqlite',
+});
+sequelize.authenticate().then(() => console.log('db is ready'));
+
+export const User = sequelize.define('User', {
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  postalCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
 const app = express();
 
 app.use(express.json());
--- src/controllers/address.ts
+++ src/controllers/address.ts
@@ -1,10 +1,12 @@
 import { addressValidate } from '../services/address-validation';
+import { User } from '../app';
 
 export async function addressController(req: any, res: any) {
   const { address, city, postalCode } = req.body;
-
   try {
+    const reqData = await User.build({ address, city, postalCode });
     const validationObject = await addressValidate(address, city, postalCode);
+    await reqData.save();
     return res.json(validationObject);
   } catch (error) {
     return res.status(500).json({ error: (error as any).message });
